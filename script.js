6f61916104ee158622389e2d4ec69d93978826a4
Lena <elena.pokhvalennaya@gmail.com>
2022-03-13 11:06:26 +0200

    updated task 1 and task 2

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
 // task 1
 
-// function sumTwoSmallestNumbers(...args) {
-//   const sortedArgs = args.sort((a, b) => a - b);
+function sumTwoSmallestNumbers(...args) {
+  if (args.length < 2) {
+    console.error("Your array hasn't 2 elements");
+    return;
+  }
 
-//   const firstElement = sortedArgs[0];
-//   const secondElement = sortedArgs[1];
-//   const sum = firstElement + secondElement;
-//   if (args.length < 2) {
-//     console.error("Your array hasn't 2 elements");
-//     return;
-//   }
-//   return sum;
-// }
+  const sortedArgs = args.sort((a, b) => a - b);
 
-// console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77));
-// console.log(sumTwoSmallestNumbers(2, 3, 0, 10, -10, 20, 16, 18, 9, 1));
-// console.log(sumTwoSmallestNumbers(10));
+  const firstElement = sortedArgs[0];
+  const secondElement = sortedArgs[1];
+  const sum = firstElement + secondElement;
+
+  return sum;
+}
+
+console.log(sumTwoSmallestNumbers(19, 5, 42, 2, 77));
+console.log(sumTwoSmallestNumbers(2, 3, 0, 10, -10, 20, 16, 18, 9, 1));
+console.log(sumTwoSmallestNumbers(10));
 
 // task 2
 
-function createCalculator(value) {
+function createCalculator(startValue) {
+  let value = startValue;
   return {
     sum: function (newValue) {
       value += newValue;
